9c911e6889cf39698531c509e8ec4816ff50eea2
primotibalt <anton.shheglov.1@gmail.com>
2026-06-29 23:23:32 +0300

    add sync most functionality

--- CheckTests/cmd/sync/server.go
+++ CheckTests/cmd/sync/server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -14,9 +16,86 @@ func RunServer() {
 	}
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /createTopic", createNewTopic)
+	mux.HandleFunc("POST /removeTopic", removeTopic)
+	mux.HandleFunc("POST /changeTopic", changeTopic)
 	http.ListenAndServe(mytailscaleip+":8081", mux)
 }
 
+func changeTopic(rw http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	createTopicModel, err := getcreatetopicmodel(r, rw)
+	if err != nil {
+		return
+	}
+	f, err := os.OpenFile(createTopicModel.Name, os.O_WRONLY|os.O_CREATE, 0o776)
+	if err != nil {
+		fmt.Println("could not open file " + createTopicModel.Name)
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer f.Close()
+	_, err = f.Write([]byte(createTopicModel.Content))
+	if err != nil {
+		fmt.Println("could not write to file " + createTopicModel.Name)
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	rw.WriteHeader(http.StatusOK)
+}
+
+func getcreatetopicmodel(r *http.Request, rw http.ResponseWriter) (CreateTopic, error) {
+	defer r.Body.Close()
+	createTopicModel := CreateTopic{}
+	allBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("could not create new topic")
+		rw.WriteHeader(http.StatusInternalServerError)
+		return createTopicModel, errors.New("Heh")
+	}
+	err = json.Unmarshal(allBytes, &createTopicModel)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("could not unmarchal payload")
+		rw.WriteHeader(http.StatusInternalServerError)
+		return createTopicModel, errors.New("Heh")
+	}
+	return createTopicModel, nil
+}
+
 func createNewTopic(rw http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello")
+	defer r.Body.Close()
+	createTopicModel, err := getcreatetopicmodel(r, rw)
+	if err != nil {
+		return
+	}
+	f, err := os.Create(createTopicModel.Name)
+	if err != nil {
+		fmt.Println("could not create topic")
+		fmt.Println(err)
+		rw.WriteHeader(http.StatusInternalServerError)
+	}
+	defer f.Close()
+	_, err = f.Write([]byte(createTopicModel.Content))
+	if err != nil {
+		fmt.Println("could not write into created file")
+		rw.WriteHeader(http.StatusInternalServerError)
+	}
+	rw.WriteHeader(http.StatusOK)
+}
+
+func removeTopic(rw http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("could not read body")
+		fmt.Println(err)
+		rw.WriteHeader(http.StatusInternalServerError)
+	}
+	filename := string(body)
+	err = os.Remove(filename)
+	if err != nil {
+		fmt.Println(filename + " does not exist in the system")
+	}
+	rw.WriteHeader(http.StatusOK)
 }
--- CheckTests/cmd/sync/watcher.go
+++ CheckTests/cmd/sync/watcher.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 )
 
+type CreateTopic struct {
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
 func AttachWatcher(path string) error {
 	tailscalepartnerip := os.Getenv("TAILSCALE_PARTNER_IP")
-	tailscalepartnerurl := tailscalepartnerip + ":8081"
+	tailscalepartnerurl := "http://" + tailscalepartnerip + ":8081"
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return err
@@ -17,17 +26,83 @@ func AttachWatcher(path string) error {
 
 	watcher.Add(path)
 	go func() {
-		select {
-		case event := <-watcher.Events:
-			switch event.Op {
-			case fsnotify.Create:
-				http.DefaultClient.Post(tailscalepartnerurl, "text", nil)
-			case fsnotify.Remove:
-			case fsnotify.Rename:
-			case fsnotify.Write:
+		for {
+			renamed := false
+			select {
+			case event := <-watcher.Events:
+				switch event.Op {
+				case fsnotify.Create:
+					createTopicModel := CreateTopic{}
+					fmt.Println("Creating...")
+					createTopicModel.Name = event.Name
+					if renamed {
+						fmt.Println("And filling...")
+						f, err := os.ReadFile(event.Name)
+						if err != nil {
+							fmt.Println(err)
+							continue
+						}
+						createTopicModel.Content = string(f)
+					}
+					body, err := json.Marshal(&createTopicModel)
+					if err != nil {
+						fmt.Printf("could not marshal %v\n", createTopicModel)
+					}
+					_, err = http.DefaultClient.Post(
+						tailscalepartnerurl+"/createTopic",
+						"application/json",
+						bytes.NewReader(body),
+					)
+					if err != nil {
+						fmt.Println(err)
+					}
+				case fsnotify.Remove:
+					fmt.Println("Removing...")
+					_, err := http.DefaultClient.Post(
+						tailscalepartnerurl+"/removeTopic",
+						"text",
+						strings.NewReader(event.Name),
+					)
+					if err != nil {
+						fmt.Println(err)
+					}
+				case fsnotify.Rename:
+					fmt.Println("Renaming...")
+					_, err := http.DefaultClient.Post(
+						tailscalepartnerurl+"/removeTopic",
+						"text",
+						strings.NewReader(event.Name),
+					)
+					if err != nil {
+						fmt.Println(err)
+					}
+				case fsnotify.Write:
+					fmt.Println("Writing...")
+					createTopicModel := CreateTopic{}
+					createTopicModel.Name = event.Name
+					f, err := os.ReadFile(event.Name)
+					if err != nil {
+						fmt.Println("could not read file " + event.Name)
+						continue
+					}
+					createTopicModel.Content = string(f)
+					body, err := json.Marshal(&createTopicModel)
+					if err != nil {
+						fmt.Println("could not marshal new changes")
+						continue
+					}
+					_, err = http.DefaultClient.Post(
+						tailscalepartnerurl+"/changeTopic",
+						"application/json",
+						bytes.NewReader(body),
+					)
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+			case err = <-watcher.Errors:
+				panic(err)
 			}
-		case err = <-watcher.Errors:
-			panic(err)
 		}
 	}()
 
